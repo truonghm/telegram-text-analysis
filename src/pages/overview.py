847031cf6923eba0803dfb6f672b79135867d77d
truonghm <truong173@outlook.com>
2020-11-02 22:43:03 +0700

    building overview page

--- src/pages/overview.py
+++ src/pages/overview.py
@@ -1,6 +1,8 @@
 
 import altair as alt
+from altair.vegalite.v4.api import selection
 import streamlit as st
+import numpy as np
 
 from src.utils import *
 
@@ -10,39 +12,54 @@ def write(state):
     with st.spinner("Loading Overview Page---"):
         st.write(f"# Overview")
     
-    # part 1
-    url = state['text_url']
-    date_started = min(state['text_df']['datetime'])
-    m1 = f"<div><span class='bold'>Date Started: <span class='highlight blue'>{date_started}</span></span></div>"
-    total_messages = len(state['text_df']['datetime'])
-    m2 = f"<div><span class='bold'>Total Messages: <span class='highlight blue'>{total_messages}</span></span></div>"
-    st.markdown(m1, unsafe_allow_html=True)
-    st.text("")
-    st.markdown(m2, unsafe_allow_html=True)
-    st.write("---")
+    col1, col2 = st.beta_columns(2)
 
-    # part 2
-    selection_year = st.selectbox(
-    "Year:",
-    ("2018", "2019", "2020")
-    )
+    with col1:
+        # part 1
+        url = state['text_url']
+        df = state['text_df']
+        date_started = min(df['datetime'])
+        m1 = f"<div><span class='bold'>Date Started: <span class='highlight blue'>{date_started}</span></span></div>"
+        total_messages = len(df['datetime'])
+        m2 = f"<div><span class='bold'>Total Messages: <span class='highlight blue'>{total_messages}</span></span></div>"
+        st.text("")
+        st.markdown(m1, unsafe_allow_html=True)
+        st.text("")
+        st.markdown(m2, unsafe_allow_html=True)
+        st.write("---")
 
-    upper=alt.Chart(url).transform_filter(
-        alt.datum.year==selection_year
-    ).mark_bar().encode(
-        y='from:N',
-        x='count():Q',
-        color='from:N'
-    )
+        # part 2
 
-    lower=alt.Chart(url).transform_filter(
-        alt.datum.year==selection_year
-    ).mark_bar().encode(
-        y='from:N',
-        x='average(word_count):Q',
-        color='from:N'
-    )
+    with col2:
+        year_list = df['year'].unique().tolist()
+        year_list.insert(0, "All")
+        year_dropdown = alt.binding_select(options=year_list)
+        year_selection = alt.selection_single(fields=['year'], bind=year_dropdown, name='Pick_a_')
 
-    c = alt.vconcat(upper, lower)
-    c
-    st.write(c)
+        base = alt.Chart(url).add_selection(
+            year_selection
+            ).transform_filter(
+            year_selection
+            )
+
+        upper=base.mark_bar().encode(
+            y='from:N',
+            x='count():Q',
+            tooltip='count():Q',
+            color=alt.Color('from:N',legend=None)
+        ).properties(
+            width=300 
+        )
+
+        lower=base.mark_bar().encode(
+            y='from:N',
+            x='average(word_count):Q',
+            tooltip='average(word_count):Q',
+            color=alt.Color('from:N',legend=None)
+        ).properties(
+            width=300 
+        )
+
+        c = alt.vconcat(upper, lower)
+        c
+        st.write(c)
